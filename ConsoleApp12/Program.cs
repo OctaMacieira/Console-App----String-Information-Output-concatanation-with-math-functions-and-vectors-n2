d80a5feff52eff7d317e6d4205be2c4397154eaa
Octa <octavio.macieira@aarim.com>
2022-08-03 20:50:59 -0300

    first commit

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, maiorAB;
+
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+
+            maiorAB = (a + b + Math.Abs(a - b)) / 2;
+
+            Console.WriteLine(maiorAB + " eh o maior");
+            Console.ReadLine();
+        }
+    }
+}
